e2fc4c6e523141c76338cca1f707c1a903b796fd
gasparshch <gasparshch@gmail.com>
2022-09-20 02:01:20 -0300

    Added bootstrap to tasks.php

--- tasks.php
+++ tasks.php
@@ -10,6 +10,7 @@ function showHome() {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
             <title>Document</title>
         </head>
         <body>
@@ -21,9 +22,9 @@ function showHome() {
             $tasks = getTasks();
             foreach($tasks as $tarea) {
                 if($tarea->finalizada == 1) {
-                    $html.= '<li> <strike>'.$tarea->titulo.': '.$tarea->descripcion.'</strike> - '.'<a href="deleteTask/'.$tarea->id_tarea.'">Borrar</a> - <a href="updateTask/'.$tarea->id_tarea.'">Done</a>'.'</li>';
+                    $html.= '<li> <strike>'.$tarea->titulo.': '.$tarea->descripcion.'</strike> - '.'<a href="deleteTask/'.$tarea->id_tarea.'" class="btn btn-danger">Borrar</a> - <a href="updateTask/'.$tarea->id_tarea.'" class="btn btn-success">Done</a>'.'</li>';
                 } else {
-                    $html.= '<li>'.'<a href="viewTask/'.$tarea->id_tarea.'">'.$tarea->titulo.'</a>'.': '.$tarea->descripcion.' - '.'<a href="deleteTask/'.$tarea->id_tarea.'">Borrar</a> - <a href="updateTask/'.$tarea->id_tarea.'">Done</a>'.'</li>';
+                    $html.= '<li>'.'<a href="viewTask/'.$tarea->id_tarea.'">'.$tarea->titulo.'</a>'.': '.$tarea->descripcion.' - '.'<a href="deleteTask/'.$tarea->id_tarea.'" class="btn btn-danger">Borrar</a> - <a href="updateTask/'.$tarea->id_tarea.'" class="btn btn-success">Done</a>'.'</li>';
 
                 }
             }
@@ -31,7 +32,7 @@ function showHome() {
             $html .='
                     </ul>
                 
-                
+                    <h2>CREAR TAREA</h2>
                     <form action="createTask" method="post">
                 
                         <input type="text" name="title" id="title">
@@ -39,10 +40,11 @@ function showHome() {
                         <input type="number" name="priority" id="priority">
                         <input type="checkbox" name="done" id="done">
                 
-                        <input type="submit" value="Guardar">
+                        <input type="submit" value="Guardar" class="btn btn-primary">
                 
                     </form>
                 
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
                 </body>
                 </html>
             ';
@@ -101,6 +103,7 @@ function viewTask($id) {
             <meta charset="UTF-8">
             <meta http-equiv="X-UA-Compatible" content="IE=edge">
             <meta name="viewport" content="width=device-width, initial-scale=1.0">
+            <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
             <title>Document</title>
         </head>
         <body>
@@ -110,6 +113,7 @@ function viewTask($id) {
             <h2> Prioridad: '.$tarea->prioridad.'</h2>
             <h2> Tarea: '.$tarea->finalizada.'</h2>
             
+            <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-u1OknCvxWvY5kfmNBILK2hRnQC3Pr17a+RTT6rIHI7NnikvbZlHgTPOOmMi466C8" crossorigin="anonymous"></script>
         </body>
         </html>';
 
